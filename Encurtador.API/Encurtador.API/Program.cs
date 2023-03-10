da1154706169d4658afb9faa8dc7e8e9cb64ca8c
OsvaldoRamos <osvaldo3378@gmail.com>
2023-03-10 20:44:31 -0300

    teste swagger

--- Encurtador.API/Encurtador.API/Program.cs
+++ Encurtador.API/Encurtador.API/Program.cs
@@ -20,12 +20,10 @@ builder.Services.RegisterRepositories();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 //app.UseHttpsRedirection();
 
